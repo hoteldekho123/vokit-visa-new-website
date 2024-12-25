c6f0e99e81b1f055d402a9bddd9bf4b0baf30616
Mohithoteldekho123 <148522843+hoteldekho123@users.noreply.github.com>
2024-12-25 17:15:12 +0530

    dfd

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,7 @@ window.onscroll = function () {
 };
 var first = true;
 function scrollFunction() {
-  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
       document.getElementById("navbar").classList.add('fixed-navbar');
       document.getElementById("scrolltop").classList.add('fixed-scroll');
   }
